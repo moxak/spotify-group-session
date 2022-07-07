b4b52a868aec004224fecfc5091310c0373f52b9
moxak <mak.1_@outlook.com>
2022-07-08 01:56:28 +0900

    0708

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,42 @@
 import { useState, useEffect } from 'react';
 import {
   BrowserRouter as Router,
-  Swtich,
   Route,
-  Link
-} from "react-router-dom";
+  Routes,
+  useLocation
+} from 'react-router-dom';
 import { accessToken, logout,  getCurrentUserProfile } from './spotify';
-import { catchErrors } from './util';
-import './App.css';
+import { catchErrors } from './utils';
+import { GlobalStyle } from './styles';
+import { Login, Profile } from './pages';
+import styled from 'styled-components/macro';
+
+const StyledLogoutButton = styled.button`
+  position: absolute;
+  top: var(--spacing-sm);
+  right: var(--spacing-md);
+  padding: var(--spacing-xs) var(--spacing-sm);
+  background-color: rgba(0,0,0,.7);
+  color: var(--white);
+  font-size: var(--fz-sm);
+  font-weight: 700;
+  border-radius: var(--border-radius-pill);
+  z-index: 10;
+  @media (min-width: 768px) {
+    right: var(--spacing-lg);
+  }
+`;
+
+
+function ScrollToTop() {
+  const { pathname }  = useLocation;
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 function App() {
   const [token, setToken] = useState(null)
@@ -23,36 +52,54 @@ function App() {
     catchErrors(fetchData());
 
   }, []);
+  function Artists() {
+    return (
+      <h1>Top Artists</h1>
+    );
+  }
+  function Tracks() {
+    return (
+      <h1>Top Tracks</h1>
+    );
+  }
+  function Playlist() {
+    return (
+      <h1>Playlist</h1>
+    );
+  }
+  function Playlists() {
+    return (
+      <h1>Playlists</h1>
+    );
+  }
+
 
   return (
     <div className="App">
+      <GlobalStyle />
       <header className="App-header">
         {!token ? (
-          <a 
-            className="App-link"
-            href="http://localhost:8888/login"
-          >
-            Login to Spotify
-          </a>
+          <Login />
         ) :(
           <>
-            <h1>Logged in!</h1>
-            <button onClick={logout}>Log Out</button>
-
-            { profile && (
-              <>
-                <h1>{ profile.display_name }</h1>
-                <p>{ profile.followers.total } Followers</p>
-                { profile.images.length && profile.images[0].url && (
-                  <img src={ profile.images[0].url } alt="Avatar" />
-                )}
-              </>
-            )}
+            <StyledLogoutButton onClick={logout}>Log Out</StyledLogoutButton>
+            <Router>
+              <ScrollToTop />
+              <Routes>
+                <Route path="/top-artists" element={<Artists />} />
+                <Route path="/top-tracks" element={<Tracks />} />
+                <Route path="/playlists/:id" element={<Playlist />} />
+                <Route path="/playlists" element={<Playlists />}  />
+                <Route path="/" element={<Profile />} />
+              </Routes>
+            </Router>
           </>
         )}
       </header>
     </div>
   );
 }
+
+
 
 export default App;
