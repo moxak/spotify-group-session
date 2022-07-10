5ecedf03765ab6d3a84e375c4e8c7c49517472c7
moxak <mak.1_@outlook.com>
2022-07-10 14:54:28 +0900

    delete intervalAction

--- client/src/components/NowPlayingBar.jsx
+++ client/src/components/NowPlayingBar.jsx
@@ -8,7 +8,7 @@ import { BsFillPlayCircleFill, BsFillPauseCircleFill } from 'react-icons/bs';
 const NowPlayingBar = () => {
     const [playbackState, setPlaybackState] = useState(null);
     const [track, setTrack] = useState(null);
-    let isActive = useMemo(() => playbackState && playbackState.is_playing ? true : false, [playbackState]);
+    const [isPlaying, setIsPlaying] = useState(false);
 
     useEffect(() => {
         const fetchData = async() => {
@@ -16,63 +16,62 @@ const NowPlayingBar = () => {
             if (status === 200) {
                 setPlaybackState(data);
                 setTrack(data.item);
+                setIsPlaying(data.is_playing);
             } else if (status === 204) {
                 setPlaybackState(null);
                 setTrack(null);
             }
         }
 
-        // Calling the function fetchData every 10 seconds.
-        setInterval(() => {
-            catchErrors(fetchData());
-        }, 10000);
+        catchErrors(fetchData());
 
     }, []);
 
     return (
         <StyledNowPlayingBar>
-            <div className={`track__contrainer ${isActive ? "playing" : ""}`}>
-                { track ? (
-                    <div className="track__item__title-group">
-                        {track.album.images.length && track.album.images[2] && (
-                            <div className="track__item__img">
-                                <img src={track.album.images[2].url} alt={track.name} />
-                            </div>
-                        )}
-                        <div className="track__item__name-artist">
-                            <div className="track__item__name overflow-ellipsis">
-                                {track.name}
-                            </div>
-                            <div className="track__item__artist overflow-ellipsis">
-                                {track.artists.map(artist => artist.name).join(', ')}
-                            </div>
+
+        <div className={`track__contrainer ${isPlaying ? "playing" : ""}`}>
+            { track ? (
+                <div className="track__item__title-group">
+                    {track.album.images.length && track.album.images[2] && (
+                        <div className="track__item__img">
+                            <img src={track.album.images[2].url} alt={track.name} />
+                        </div>
+                    )}
+                    <div className="track__item__name-artist">
+                        <div className="track__item__name overflow-ellipsis">
+                            {track.name}
+                        </div>
+                        <div className="track__item__artist overflow-ellipsis">
+                            {track.artists.map(artist => artist.name).join(', ')}
                         </div>
                     </div>
-                ) : (
-                    <div>Spotify is not active</div>
-                )}
-                
-                <div className="playling__bar">
-                    <button 
-                        onClick={()=> {
-                            if (isActive) {
-                                isActive = false;
-                                pausePlayback();
-                            } else {
-                                isActive = true;
-                                startPlayback();
-                            }
-                        }}
-                    >
-                    {isActive ? (<BsFillPauseCircleFill size={42}/>) : (<BsFillPlayCircleFill size={42}/>)}
-                    </button>
                 </div>
-                <div className="playling__volume">
-                    { isActive ? (<AudioSpectrum />): null }
-                </div>
+            ) : (
+                <div>Spotify is not active</div>
+            )}
+            
+            <div className="playling__bar">
+                <button 
+                    onClick={()=> {
+                        if (isPlaying) {
+                            setIsPlaying(false);
+                            pausePlayback();
+                        } else {
+                            setIsPlaying(true);
+                            startPlayback();
+                        }
+                    }}
+                >
+                {isPlaying ? (<BsFillPauseCircleFill size={42}/>) : (<BsFillPlayCircleFill size={42}/>)}
+                </button>
             </div>
+            <div className="playling__volume">
+                { isPlaying ? (<AudioSpectrum />): null }
+            </div>
+        </div>
 
-        </StyledNowPlayingBar>
+    </StyledNowPlayingBar>
     );
 };
 
